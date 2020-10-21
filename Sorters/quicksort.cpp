e9075b33a34f81bd96889da9de308a4574d6cb36
Davi Cardoso <davi.almeida.c@gmail.com>
2020-10-21 00:48:34 -0300

    added quickSort method

--- Sorters/quicksort.cpp
+++ Sorters/quicksort.cpp
@@ -1,4 +1,56 @@
 #include "sorters.h"
+#include "quicksort.h"
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
 void quicksort(Book **to_sort, int size, int swap_count, int compare_count) {
 
+    sort(to_sort, 0, size - 1);  
+    printArray(to_sort,size);
 }
+
+void sort(Book **to_sort, int low, int high)  
+{  
+    if (low < high)  
+    {  
+        int pi = partition(to_sort, low, high);  
+  
+        sort(to_sort, low, pi - 1);  
+        sort(to_sort, pi + 1, high);  
+    }  
+}  
+ 
+int partition (Book **to_sort, int low, int high)  
+{  
+    Book *pivot = to_sort[high]; // pivot  
+    int i = (low - 1); // Index of smaller element  
+  
+    for (int j = low; j <= high - 1; j++)  
+    {  
+        // If current element is smaller than the pivot  
+        if (to_sort[j] < pivot)  
+        {  
+            i++; // increment index of smaller element  
+            swap(&to_sort[i], &to_sort[j]);  
+        }  
+    }  
+    swap(&to_sort[i + 1], &to_sort[high]);  
+    return (i + 1);  
+}
+
+void swap(int* a, int* b)  
+{  
+    int t = *a;  
+    *a = *b;  
+    *b = t;  
+} 
+
+void printArray(Book **to_sort, int size)  
+{  
+    int i;  
+    for (i = 0; i < size; i++)  
+        cout << to_sort[i] << " ";  
+    cout << endl;  
+}  
---
+++ Sorters/quicksort.h
@@ -0,0 +1,7 @@
+void sort(int arr[], int low, int high);  
+
+void printArray(int arr[], int size);  
+
+void swap(int* a, int* b);  
+  
+int partition (int arr[], int low, int high);  
