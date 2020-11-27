46458fdb3b2cd228f1438588b96a2b04b38c5737
Davi Cardoso <davi.almeida.c@gmail.com>
2020-11-27 20:01:27 -0300

    searchRBTree

--- Trees/RBTree.cpp
+++ Trees/RBTree.cpp
@@ -633,3 +633,8 @@ void insertRBTree(Book **to_sort, int size, int copy_count, int compare_count) {
     }
    
 }
+
+void searchRBTree(Book **to_sort, int size, int copy_count, int compare_count) {
+    
+    cout<<"Inserting "<<endl<<endl;  
+}
